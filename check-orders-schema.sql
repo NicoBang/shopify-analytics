9625bfea28ac6c86115b3ddd3db9181f24db2bfe
Nicolai Bang <nb@8kilo.dk>
2025-10-15 10:39:48 +0200

    CLEANUP

--- check-orders-schema.sql
+++
@@ -1,8 +0,0 @@
--- Check the orders table schema
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'orders'
-ORDER BY ordinal_position;
-
--- If cancelled_at column doesn't exist, add it:
--- ALTER TABLE orders ADD COLUMN IF NOT EXISTS cancelled_at TIMESTAMPTZ;
